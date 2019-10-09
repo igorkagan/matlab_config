1e21d64297bc959b161659811ebba58c90d63848
Igor Kagan <igor.b.kagan@gmail.com>
2019-10-09 12:48:15 +0200

    Update startup.m

--- MATLAB2014/startup.m
+++ MATLAB2014/startup.m
@@ -1,5 +1,5 @@
 disp('Welcome to MATLAB2014');
 set_sources_path;
 edit;
-EditorMacro('Alt-Control-h', @ig_createHeaderComment_dag);
+%EditorMacro('Alt-Control-h', @ig_createHeaderComment_dag);
 dbstop if error
