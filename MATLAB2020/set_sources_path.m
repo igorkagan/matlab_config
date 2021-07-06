4f04d093acb8ebc1dfbd443b1a4c3c66c2ff32c9
Igor Kagan <igor.b.kagan@gmail.com>
2021-07-06 12:31:01 +0200

    MATLAB2020

---
+++ MATLAB2020/set_sources_path.m
@@ -0,0 +1,5 @@
+if 1
+        addpath(genpath_exclude('E:\Dropbox\Sources\Repos',{'.git','neuroelf-matlab'})); 
+        rmpath('E:\Dropbox\Sources\Repos\NaN')
+end
+
---
+++ MATLAB2020/startup.m
@@ -0,0 +1,5 @@
+disp('Welcome to MATLAB2020');
+set_sources_path;
+edit;
+%EditorMacro('Alt-Control-h', @ig_createHeaderComment_dag);
+dbstop if error
